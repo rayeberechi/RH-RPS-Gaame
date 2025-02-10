6887872e65785b6c1adbdbcbc3a8157bc844b42e
rayeberechi <faitheberechiraymond@gmail.com>
2025-02-10 12:06:58 +0100

    Fixed icon/message display on result page indicating if player won, loss or its a tie

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const rulesBtn = document.querySelector(".rules-btn");
     const rulesDisplay = document.querySelector(".rules-display");
     const closeBtn = document.querySelector(".close-btn");
+    const winDiv = document.querySelector(".win");
+    const lossDiv = document.querySelector(".loss");
+    const drawDiv = document.querySelector(".draw");
 
     let score = 0;
     const choices = ["rock", "paper", "scissors"];
@@ -33,6 +36,10 @@ document.addEventListener("DOMContentLoaded", () => {
     function displayResult(player, computer) {
         mainPage.style.display = "none";
         resultTab.style.display = "block";
+        winDiv.style.display = "none";
+        lossDiv.style.display = "none";
+        drawDiv.style.display = "none";
+
         playerChoiceText.innerText = `You Chose: ${player}`;
         computerChoiceText.innerText = "Computer Choosing...";
 
@@ -40,6 +47,14 @@ document.addEventListener("DOMContentLoaded", () => {
             computerChoiceText.innerText = `Computer Chose: ${computer}`;
             const result = getWinner(player, computer);
             updateScore(result);
+
+            if (result === "player") {
+                winDiv.style.display = "block";
+            } else if (result === "computer") {
+                lossDiv.style.display = "block";
+            } else {
+                drawDiv.style.display = "block";
+            }
         }, 2000);
     }
 
@@ -49,8 +64,9 @@ document.addEventListener("DOMContentLoaded", () => {
             (player === "paper" && computer === "rock") ||
             (player === "scissors" && computer === "paper")) {
             return "player";
-        }
+        } else {
         return "computer";
+        }
     }
 
     function updateScore(winner) {
@@ -68,6 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     quitBtn.addEventListener("click", () => {
+        alert("Thanks for playing!");
         resultTab.style.display = "none";
         welcomePage.style.display = "block";
         score = 0;
